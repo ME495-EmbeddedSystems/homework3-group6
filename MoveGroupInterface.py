5c9c939ecd2770d63ddcbefd0f120717820841c0
Luke Batteas <LukeBatteas2027@u.northwestern.edu>
2023-10-31 00:25:40 -0500

    Motions plan! Trajectories Execute! Code is not commented! At all! To be fixed next time. Need to add some final functionality. Also times out with postion constraint but TBD

--- MoveGroupInterface.py
+++ MoveGroupInterface.py
@@ -1,11 +1,12 @@
 import rclpy
 from rclpy.node import Node
+import asyncio
 
 from rclpy.action import ActionClient
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 
 # https://wiki.ros.org/moveit_msgs
-from moveit_msgs.msg import PlannerParams, MotionPlanRequest, WorkspaceParameters, Constraints, RobotState, PositionConstraint, OrientationConstraint, JointConstraint, BoundingVolume, PlanningOptions
+from moveit_msgs.msg import PlannerParams, MotionPlanRequest, WorkspaceParameters, Constraints, RobotState, PositionConstraint, OrientationConstraint, JointConstraint, BoundingVolume, PlanningOptions, RobotTrajectory
 from moveit_msgs.srv import GetPlannerParams, SetPlannerParams, GraspPlanning, QueryPlannerInterfaces, GetCartesianPath
 from moveit_msgs.action import MoveGroup, ExecuteTrajectory
 
@@ -14,6 +15,8 @@ from shape_msgs.msg import SolidPrimitive
 
 from sensor_msgs.msg import JointState
 from enum import Enum, auto
+
+from action_msgs.msg import GoalStatus
 
 
 import tf2_ros
@@ -34,13 +37,29 @@ class MoveApi(Node):
         self.API.setWorkspaceParamaters('panda_link0',-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
         self.API.setPlanningPipelineID("move_group")
 
-        posGoal = PoseStamped()
-        posGoal.header.frame_id = "panda_link0"
-        self.get_logger().info("Here1")
-        self.API.addOrientationConstraint(posGoal)
-        self.get_logger().info("Here2")
-        res = self.API.makeMotionPlanRequest()
-        self.get_logger().info(res.error_code)
+        poseGoal = PoseStamped()
+        poseGoal.header.frame_id = "panda_link0"
+        poseGoal.pose.position.z = 0.3
+        self.API.addOrientationConstraint(poseGoal)
+        self.API.addPositionConstraint(poseGoal)
+        
+        self.res = None
+        self.traj = None
+        
+        tmr = self.create_timer(0.01, self.timer_callback)
+        self.i = 0
+
+    async def timer_callback(self):
+        self.i +=1
+        if(self.i == 100):
+            self.res, self.stat1 = await self.API.makeMotionPlanRequest(plan_only=True)
+            self.traj = self.res.planned_trajectory
+        
+        if(not(self.traj is None) and self.i == 300):
+            self.res2, self.stat2 = await self.API.executeTrajectory(self.traj)
+           
+            
+        
 
 
         
@@ -96,30 +115,19 @@ class MoveGroupInterface():
         self.joint_names_ = robotModel.joint_names
         self.end_effector_link_ = robotModel.default_end_effector
 
-        self.namespace_ = namespace
+        self.namespace_ = namespace #Nice option but also unused 
         self.node_ = node
-        self.tf_buffer_ = tf_buffer
+        self.tf_buffer_ = tf_buffer # Unsued? Why is it in the c++ version?
         self.wait_for_servers_ = wait_for_servers
         
         self.logger_ = self.node_.get_logger()
         self.clock_ = self.node_.get_clock()
-        self.cb_group_ = ReentrantCallbackGroup()
+        self.cb_group_ = ReentrantCallbackGroup() # Change to MECB probablly 
 
-        # Available options, can redo / ignore some
-        # opt.group_name 
-        # opt.robot_model
-        # opt.robot_description
-        # opt.namespace
-
-        # self.pose_targets_
-        # self.end_effector_link_
-        # self.pose_reference_frame_
-        # self.target_type_
-        # self.start_state_
 
         self.planning_pipeline_id_ = ""
         self.planner_id_ = ""
-        self.workspace_parameters_ = WorkspaceParameters()
+        self.workspace_parameters_ = None
 
         self.can_look_ = False
         self.look_around_attempts_ = 0
@@ -129,12 +137,13 @@ class MoveGroupInterface():
         self.goal_joint_tolerance_ = 1e-4
         self.goal_position_tolerance_ = 1e-4
         self.goal_orientation_tolerance_ = 1e-3
-        self.allowed_planning_time_ = 5.0
+        self.allowed_planning_time_ = 15.0
         self.num_planning_attemps_ = 1
 
         self.current_state_ = None
         self.start_state_ = None
-        
+
+        # !!! Add setters / getters for this, 
         self.max_velocity_scaling_factor_ = 0.1 #self.node_.get_parameter("velocity_scaling_factor").get_parameter_value().double_value
         self.max_acceleration_scaling_factor_ = 0.1 #self.node_.get_parameter("acceleration_scaling_factor").get_parameter_value().double_value
 
@@ -146,9 +155,8 @@ class MoveGroupInterface():
         self.orientation_constraints_ = []
         self.joint_constraints_ = []
 
-        # Subscribe to /joint_states
 
-        self.joint_state_sub = self.node_.create_subscription(JointState, "/joint_state", self.joint_state_callback, 10)
+        self.joint_state_sub = self.node_.create_subscription(JointState, "/joint_states", self.joint_state_callback, 10)
 
         self.move_action_client_ = ActionClient(self.node_, MoveGroup, self.namespace_ + "/move_action", callback_group=self.cb_group_)
         if not self.move_action_client_.wait_for_server(timeout_sec=self.wait_for_servers_):
@@ -174,6 +182,14 @@ class MoveGroupInterface():
         if not self.cartesian_path_service_.wait_for_service(timeout_sec=self.wait_for_servers_):
             raise RuntimeError("Timeout waiting for compute_cartesian_path service")
 
+
+    """
+
+    Setters / Getters 
+    !!! (Sort them? )
+
+    """
+
     def setPlannerParams(self, planner_id, group, params, replace=False):
         request = SetPlannerParams()
         request.planner_config = planner_id
@@ -191,6 +207,7 @@ class MoveGroupInterface():
 
         if(future_gpp_response.done()):
             return future_gpp_response.get()
+    
     def setPlanningPipelineID(self, pipeline_id):
         if isinstance(pipeline_id, str):
             if(pipeline_id != self.planning_pipeline_id_):
@@ -202,6 +219,7 @@ class MoveGroupInterface():
         return self.planning_pipeline_id_
     
     def getDefaultPlannerID(self):
+        # !!! Fix this. Possible in Ros2 but more annoying
         default_planner_id = self.node_.get_parameter("move_group/default_planning_pipeline").get_parameter_value().string_value
         return default_planner_id
 
@@ -306,20 +324,50 @@ class MoveGroupInterface():
     def setStartState(self, joint_values, mdof_joint_values=None, attached_objects=None, is_diff = None):
         # !!! Fix this
         self.start_state_ = RobotState()
-        self.start_state_ .joint_states = joint_values
+        self.start_state_.joint_state = joint_values
         if(not(mdof_joint_values is None)):
             self.start_state_ .multi_dof_joint_state = mdof_joint_values
         if(not(attached_objects is None)):
             self.start_state_ .attached_collision_objects = attached_objects
         if(not(is_diff is None)):
             self.start_state_ .is_diff = is_diff
+    
+    def setWorkspaceParamaters(self, frame, minx, maxx, miny, maxy, minz, maxz):
+        if((isinstance(minx, int) or isinstance(minx, float) ) and
+           (isinstance(miny, int) or isinstance(miny, float) ) and
+           (isinstance(minz, int) or isinstance(minz, float) ) and
+           (isinstance(maxx, int) or isinstance(maxx, float) ) and
+           (isinstance(maxy, int) or isinstance(maxy, float) ) and
+           (isinstance(maxz, int) or isinstance(maxz, float) )):
+
+            self.workspace_parameters_ = WorkspaceParameters()
+            self.workspace_parameters_.header.frame_id = frame
+            self.workspace_parameters_.header.stamp = self.clock_.now().to_msg()
+
+            self.workspace_parameters_.min_corner.x = minx
+            self.workspace_parameters_.min_corner.y = miny
+            self.workspace_parameters_.min_corner.z = minz
+
+            self.workspace_parameters_.max_corner.x = maxx
+            self.workspace_parameters_.max_corner.y = maxy
+            self.workspace_parameters_.max_corner.z = maxz
+        else:
+            self.logger_().error("Attempt to set workspace parameter coordiante to invalid type")
+        
+    
+    """
+
+    Constraint Functions
+
+    """
 
     def addPositionConstraint(self, pose_stamped, link=None, tolerance=None, weight=1.0):
         new_pc = PositionConstraint()
         new_pc.header = pose_stamped.header
 
-        # This may cause an error because position is a point msg and offset is a vector3f
-        new_pc.target_point_offset = pose_stamped.pose.position
+        new_pc.target_point_offset.x = pose_stamped.pose.position.x
+        new_pc.target_point_offset.y = pose_stamped.pose.position.y
+        new_pc.target_point_offset.z = pose_stamped.pose.position.z
 
         if(link is None):
             new_pc.link_name = self.end_effector_link_
@@ -331,11 +379,9 @@ class MoveGroupInterface():
 
         BV = BoundingVolume()
         
-        SP = SolidPrimitive
+        SP = SolidPrimitive()
         SP.type = 1
-        SP.dimensions[0] = (2 * tolerance)
-        SP.dimensions[1] = (2 * tolerance)
-        SP.dimensions[2] = (2 * tolerance)
+        SP.dimensions = [2 * tolerance, 2 * tolerance, 2 * tolerance]
 
         BV.primitives =  [SP]
         BV.primitive_poses = [pose_stamped.pose]
@@ -346,7 +392,7 @@ class MoveGroupInterface():
         self.position_constraints_.append(new_pc)
 
     def clearPositionConstraints(self):
-        self.position_constraints_ = None
+        self.position_constraints_ = []
 
     def addOrientationConstraint(self, pose_stamped, link=None, tolerance=None, weight = 1.0):
         new_oc = OrientationConstraint()
@@ -367,32 +413,27 @@ class MoveGroupInterface():
             new_oc.absolute_z_axis_tolerance = tolerance
         
         new_oc.weight = weight
-
         self.orientation_constraints_.append(new_oc)
 
     def clearOrientationConstraints(self):
-        self.orientation_constraints_ = None
+        self.orientation_constraints_ = []
+    
+    def addPoseConstraint(self, pose_stamped, link=None, lin_tol=None, ang_tol=None, weight = 1.0):
+        self.addOrientationConstraint(pose_stamped, link=link, tolerance=ang_tol, weight=weight)
+        self.addPositionConstraint(pose_stamped, link=link, tolerance=lin_tol, weight=weight)
 
+    def clearPoseConstraints(self):
+        self.clearOrientationConstraints()
+        self.clearPositionConstraints()
+    
     # Add joint_constraint functionality
 
-    def constructMotionPlanRequest(self):
-        request = MotionPlanRequest()
-        request.group_name = self.group_name_
-        request.num_planning_attempts = self.num_planning_attemps_
-        request.max_velocity_scaling_factor = self.max_velocity_scaling_factor_
-        request.max_acceleration_scaling_factor = self.max_acceleration_scaling_factor_
-        request.allowed_planning_time = self.allowed_planning_time_
-        request.pipeline_id = self.planning_pipeline_id_
-        request.planner_id = self.planner_id_
-        request.workspace_parameters = self.workspace_parameters_
-        request.start_state = self.start_state_
-        self.constraints_.position_constraints = self.position_constraints_
-        self.constraints_.orientation_constraints = self.orientation_constraints_
-
-        # Fix This !--!
-        #self.constraints_ = self.mergeConstraints(self.constraints_, self.constraints_)
-        request.goal_constraints = [self.constraints_]
-        return request
+    def clearJointConstraints(self):
+        self.joint_constraints_ = []
+    
+    def clearAllConstraints(self):
+        self.clearPoseConstraints()
+        self.clearJointConstraints()  
     
     def mergeConstraints(self, first_c, second_c):
         # !!! Fix this
@@ -442,26 +483,93 @@ class MoveGroupInterface():
             merged_c.visibility_constraints.append(i)
         
         return(merged_c)
+    
+    """
 
-    def makeMotionPlanRequest(self, start_state = None, eef_pose_stamped=None):
+    Action functions
+
+    """
+
+    def constructPlannerOptions(self, plan_only=True):
+        options = PlanningOptions()
+        options.plan_only = plan_only
+        options.look_around = self.can_look_
+        options.look_around_attempts = self.look_around_attempts_
+        options.replan = self.can_replan_
+        options.replan_attempts = self.replan_attempts_
+        options.replan_delay = self.replan_delay_
+        return options
+
+
+    def constructMotionPlanRequest(self):
+        request = MotionPlanRequest()
+        request.group_name = self.group_name_
+        request.num_planning_attempts = self.num_planning_attemps_
+        request.max_velocity_scaling_factor = self.max_velocity_scaling_factor_
+        request.max_acceleration_scaling_factor = self.max_acceleration_scaling_factor_
+        request.allowed_planning_time = self.allowed_planning_time_
+        request.pipeline_id = self.planning_pipeline_id_
+        request.planner_id = self.planner_id_
+        request.workspace_parameters = self.workspace_parameters_
+        request.start_state = self.start_state_
+        self.constraints_.position_constraints = self.position_constraints_
+        self.constraints_.orientation_constraints = self.orientation_constraints_
+
+        # !!! Fix this function 
+        #self.constraints_ = self.mergeConstraints(self.constraints_, self.constraints_)
+        request.goal_constraints = [self.constraints_]
+        return request
+    
+
+    async def makeMotionPlanRequest(self, plan_only=True, start_state = None, eef_pose_stamped=None, clean=True):
         if(not(eef_pose_stamped is None)):
             self.addPositionConstraint(eef_pose_stamped)
             self.addOrientationConstraint(eef_pose_stamped)
-        # Fix this !--!
-        #if(not(start_state is None) and self.start_state_ is None):
-        #   self.setStartState(self.current_state_)
-        self.start_state_ = RobotState()
-        goal = MoveGroup.Goal()
-        # ADD PLANNING OPTIONS SECTION
-        goal.request = self.constructMotionPlanRequest()
-        call_ma_future = self.move_action_client_.send_goal_async(goal)
-        self.start_state_ = None
-        self.clearOrientationConstraints()
-        self.clearPositionConstraints()
-        while not(call_ma_future.done()):
-            self.logger_.info("Waiting!")
-        return call_ma_future.result()
+
+        if((start_state is None and self.start_state_ is None) or not(plan_only)):
+            self.setStartState(self.current_state_)
+        elif(not(start_state is none)):
+            self.setStartState(start_state)
         
+        goal = MoveGroup.Goal()
+        goal.planning_options = self.constructPlannerOptions(plan_only)
+        goal.request = self.constructMotionPlanRequest()
+        goal_handle = await self.move_action_client_.send_goal_async(goal)
+
+        if not goal_handle.accepted:
+            self.logger_.info("Goal not accepted")
+            return
+        self.logger_.info("Goal accepted.")
+
+        res = await goal_handle.get_result_async()
+        result = res.result
+        status = res.status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.logger_.info("Goal succeeded!")
+        else:
+            self.logger_.info('Goal failed with status: {0}'.format(status))
+        if(clean):
+            self.cleanUp()
+        return result, status
+    
+    async def executeTrajectory(self, trajectory):
+        goal = ExecuteTrajectory.Goal()
+        goal.trajectory = trajectory
+        goal_handle = await self.execute_action_client_.send_goal_async(goal)
+
+        if not goal_handle.accepted:
+            self.logger_.info("Goal not accepted")
+            return
+        self.logger_.info("Goal accepted.")
+
+        res = await goal_handle.get_result_async()
+        result = res.result
+        status = res.status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.logger_.info("Goal succeeded!")
+        else:
+            self.logger_.info('Goal failed with status: {0}'.format(status))
+        return result, status
     
     def joint_state_callback(self, msg):
         self.current_state_ = JointState()
@@ -475,30 +583,10 @@ class MoveGroupInterface():
                     self.current_state_.velocity.append(msg.velocity[i])
                     self.current_state_.effort.append(msg.effort[i])
 
-    
-    def setWorkspaceParamaters(self, frame, minx, maxx, miny, maxy, minz, maxz):
-        if((isinstance(minx, int) or isinstance(minx, float) ) and
-           (isinstance(miny, int) or isinstance(miny, float) ) and
-           (isinstance(minz, int) or isinstance(minz, float) ) and
-           (isinstance(maxx, int) or isinstance(maxx, float) ) and
-           (isinstance(maxy, int) or isinstance(maxy, float) ) and
-           (isinstance(maxz, int) or isinstance(maxz, float) )):
-
-            self.workspace_parameters_.header.frame_id = frame
-            self.workspace_parameters_.header.stamp = self.clock_.now().to_msg()
-
-            self.workspace_parameters_.min_corner.x = minx
-            self.workspace_parameters_.min_corner.y = miny
-            self.workspace_parameters_.min_corner.z = minz
-
-            self.workspace_parameters_.max_corner.x = maxx
-            self.workspace_parameters_.max_corner.y = maxy
-            self.workspace_parameters_.max_corner.z = maxz
-        else:
-            self.logger_().error("Attempt to set workspace parameter coordiante to invalid type")
-    
-
-
+    def cleanUp(self):
+        """Clean up after planning"""
+        self.clearAllConstraints()
+        self.start_state_ = None
 
 
 def main(args=None):
